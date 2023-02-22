79341ea9df980bfabd59b9d734ad0cf0f345beeb
Devanshu Chatterjee <devanshu4190@gmail.com>
2023-02-23 00:50:00 +0530

    checking all exports in rde

--- src/pages/CanvaEditor.jsx
+++ src/pages/CanvaEditor.jsx
@@ -2,11 +2,13 @@ import React from "react"
 import EditorLayout from "../layouts/EditorLayout"
 import * as RDE from "react-design-editor"
 
+const t = RDE.CanvasObject
+
 const CanvaEditor = () => {
 	return (
 		<EditorLayout>
 			<div>
-				<RDE.Canvas  />
+				<RDE.Canvas />
 			</div>
 		</EditorLayout>
 	)
