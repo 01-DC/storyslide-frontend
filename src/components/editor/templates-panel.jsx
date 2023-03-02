98a9075e1c388804754d1e3dfdf2b43334585eb7
shukapurv <apoorveshukla2001@gmail.com>
2023-03-02 05:53:57 +0530

    added polotno library

---
+++ src/components/editor/templates-panel.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { observer } from 'mobx-react-lite';
+import { useInfiniteAPI } from 'polotno/utils/use-api';
+
+import { SectionTab } from 'polotno/side-panel';
+import MdPhotoLibrary from '@meronex/icons/md/MdPhotoLibrary';
+
+import { ImagesGrid } from 'polotno/side-panel/images-grid';
+
+export const TemplatesPanel = observer(({ store }) => {
+    // load data
+    const { data, isLoading } = useInfiniteAPI({
+        getAPI: ({ page }) => `templates/page${page}.json`,
+    });
+
+    return (
+        <div style={{ height: '100%' }}>
+            <ImagesGrid
+                shadowEnabled={false}
+                images={data?.map((data) => data.items).flat()}
+                getPreview={(item) => `/templates/${item.preview}`}
+                isLoading={isLoading}
+                onSelect={async (item) => {
+                    // download selected json
+                    const req = await fetch(`/templates/${item.json}`);
+                    const json = await req.json();
+                    // just inject it into store
+                    store.loadJSON(json);
+                }}
+                rowsNumber={1}
+            />
+        </div>
+    );
+});
+
+// define the new custom section
+export const TemplatesSection = {
+    name: 'custom-templates',
+    Tab: (props) => (
+        <SectionTab name="Custom templates" {...props}>
+            <MdPhotoLibrary />
+        </SectionTab>
+    ),
+    // we need observer to update component automatically on any store changes
+    Panel: TemplatesPanel,
+};
--- src/pages/CanvaEditor.jsx
+++ src/pages/CanvaEditor.jsx
@@ -1,11 +1,69 @@
-import React from "react"
+import React from 'react';
+import { useEffect } from 'react';
+
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/popover2/lib/css/blueprint-popover2.css';
+
+import { PolotnoContainer, SidePanelWrap, WorkspaceWrap } from 'polotno';
+import { Workspace } from 'polotno/canvas/workspace';
+import { SidePanel } from 'polotno/side-panel';
+import { Toolbar } from 'polotno/toolbar/toolbar';
+import { ZoomButtons } from 'polotno/toolbar/zoom-buttons';
+import { createStore } from 'polotno/model/store';
+
+import { DEFAULT_SECTIONS } from 'polotno/side-panel';
+import { TemplatesSection } from '../components/editor/templates-panel';
 import EditorLayout from "../layouts/EditorLayout"
+
+const store = createStore({
+	// this is a demo key just for that project
+	// (!) please don't use it in your projects
+	// to create your own API key please go here: https://polotno.com/cabinet
+	key: 'nFA5H9elEytDyPyvKL7T',
+
+});
+
+store.addPage();
+// console.log(data)
+// store.loadJSON(data)
+
+// store.setRole('admin');
+
+const sections = [TemplatesSection, ...DEFAULT_SECTIONS];
+
+const exportData = () => {
+	const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+		JSON.stringify(store.toJSON())
+	)}`;
+	const link = document.createElement("a");
+	link.href = jsonString;
+	link.download = "data.json";
+
+	link.click();
+};
 
 const CanvaEditor = () => {
 	return (
 		<EditorLayout>
-			<div>Editor</div>
+			<PolotnoContainer style={{ width: "100vw", height: "100vh" }}>
+				<SidePanelWrap>
+					<SidePanel
+						store={store}
+						sections={sections}
+						defaultSection="custom-templates"
+					/>
+				</SidePanelWrap>
+				<WorkspaceWrap >
+
+					<Toolbar store={store} downloadButtonEnabled />
+					<Workspace store={store} />
+					<ZoomButtons store={store} />
+				</WorkspaceWrap>
+			</PolotnoContainer>
 		</EditorLayout>
+
+
 	)
 }
 
