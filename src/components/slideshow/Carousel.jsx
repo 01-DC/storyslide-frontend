a55d5972c65dd607d6b109d4e73b47db0b0d6c9e
Devanshu Chatterjee <devanshu4190@gmail.com>
2023-03-06 16:39:29 +0530

    created slider and top indicator

--- src/components/slideshow/Carousel.jsx
+++ src/components/slideshow/Carousel.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react"
 import { AnimatePresence, motion } from "framer-motion"
-// import "./carousel.scss"
 import { useEffect } from "react"
+import eye from "../../assets/Groupeye.svg"
+
 const Carousel = ({ data }) => {
   const [flowDirection, setFlowDirection] = useState(true)
   const [centerId, setCenterId] = useState(0)
   const [leftId, setLeftId] = useState(data.length - 1)
   const [rightId, setRightId] = useState(1)
+  const [cb1, setCB1] = useState(true)
+  const [cb2, setCB2] = useState(false)
+  const [cb3, setCB3] = useState(false)
 
   const nextBtn = () => {
     if (leftId === data.length - 1) {
@@ -19,12 +23,26 @@ const Carousel = ({ data }) => {
     } else {
       setCenterId(centerId + 1)
     }
-
     if (rightId === data.length - 1) {
       setRightId(0)
     } else {
       setRightId(rightId + 1)
     }
+
+    if (cb1) {
+      setCB1(false)
+      setCB2(true)
+      setCB3(false)
+    } else if (cb2) {
+      setCB1(false)
+      setCB2(false)
+      setCB3(true)
+    } else {
+      setCB1(true)
+      setCB2(false)
+      setCB3(false)
+    }
+
     setFlowDirection(true)
   }
 
@@ -44,6 +62,20 @@ const Carousel = ({ data }) => {
       setRightId(data.length - 1)
     } else {
       setRightId(rightId - 1)
+    }
+
+    if (cb1) {
+      setCB1(false)
+      setCB2(false)
+      setCB3(true)
+    } else if (cb2) {
+      setCB1(true)
+      setCB2(false)
+      setCB3(false)
+    } else {
+      setCB1(false)
+      setCB2(true)
+      setCB3(false)
     }
   }
 
@@ -68,13 +100,13 @@ const Carousel = ({ data }) => {
       },
     },
     left: {
-      x: "-100%",
+      x: "-300px",
       opacity: 1,
       filter: "brightness(40%)",
       scale: 1,
       backgroundImage: "url(" + data[leftId] + ")",
       zIndex: "4",
-      boxShadow: "unset",
+      boxShadow: "0px 0px 30px 0px rgba(0,0,0,0.0)",
       transition: {
         type: "spring",
         duration: 1,
@@ -82,11 +114,11 @@ const Carousel = ({ data }) => {
     },
     right: {
       backgroundImage: "url(" + data[rightId] + ")",
-      x: "100%",
+      x: "300px",
       opacity: 1,
       filter: "brightness(40%)",
       scale: 1,
-      boxShadow: "unset",
+      boxShadow: "0px 0px 30px 0px rgba(0,0,0,0.0)",
       zIndex: "3",
       transition: {
         type: "spring",
@@ -94,77 +126,124 @@ const Carousel = ({ data }) => {
       },
     },
     rightHidden: {
-      x: "150%",
+      x: "400px",
       scale: 0,
       opacity: 0,
     },
     leftHidden: {
-      x: "-150%",
+      x: "-400px",
       scale: 0,
       opacity: 0,
     },
   }
 
+  const topBar = []
   return (
-    <motion.div className="grid place-content-center rounded-2xl h-screen">
-      <motion.div className="relative h-96 aspect-[9/16]">
-        <AnimatePresence initial={false}>
-          <motion.div
-            key={leftId}
-            variants={variants}
-            initial={flowDirection ? "center" : "leftHidden"}
-            animate="left"
-            exit={"leftHidden"}
-            className="absolute h-96 aspect-[9/16] bg-center bg-cover rounded-2xl bg-no-repeat"
-          ></motion.div>
-          <motion.div
-            variants={variants}
-            key={centerId}
-            initial={flowDirection ? "right" : "left"}
-            animate="center"
-            className="absolute h-96 aspect-[9/16] bg-center bg-cover rounded-2xl bg-no-repeat"
-          ></motion.div>
-          <motion.div
-            key={rightId}
-            variants={variants}
-            initial={flowDirection ? "rightHidden" : "center"}
-            animate="right"
-            exit={"rightHidden"}
-            className="absolute h-96 aspect-[9/16] bg-center bg-cover rounded-2xl bg-no-repeat"
-          ></motion.div>
-        </AnimatePresence>
-      </motion.div>
-      <div className="flex justify-center gap-4 z-10">
-        <motion.button
-          initial={{ opacity: 0, scale: 0 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            type: "spring",
-            duration: 0.5,
-          }}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.8 }}
-          className="bwd-btn w-20 bg-slate-700 text-white font-bold"
-          onClick={prevBtn}
-        >
-          Back
-        </motion.button>
-        <motion.button
-          initial={{ opacity: 0, scale: 0 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            type: "spring",
-            duration: 0.5,
-          }}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.8 }}
-          className="fwd-btn w-20 bg-slate-700 text-white font-bold"
-          onClick={nextBtn}
-        >
-          Next
-        </motion.button>
+    <div className="h-screen overflow-hidden">
+      <div className="flex items-center justify-center pt-8 px-8 gap-4">
+        {data.map((d, i) => (
+          <div
+            className={`h-1 w-full rounded-md ${
+              i <= centerId ? "bg-white" : "bg-gray-800"
+            }`}
+          ></div>
+        ))}
       </div>
-    </motion.div>
+
+      <div className="flex justify-between px-8 mt-4">
+        <h1 className="font-bold text-3xl text-white">Marketing Pitch</h1>
+        <div className="text-white flex items-center gap-2">
+          <img src={eye} /> 32
+        </div>
+      </div>
+
+      <motion.div className="grid place-content-center rounded-2xl h-screen">
+        <motion.div className="relative h-96 aspect-[9/16]">
+          <AnimatePresence initial={false}>
+            <motion.div
+              key={leftId}
+              variants={variants}
+              initial={flowDirection ? "center" : "leftHidden"}
+              animate="left"
+              exit={"leftHidden"}
+              className="absolute h-96 aspect-[9/16] bg-center bg-cover rounded-2xl bg-no-repeat"
+            ></motion.div>
+            <motion.div
+              variants={variants}
+              key={centerId}
+              initial={flowDirection ? "right" : "left"}
+              animate="center"
+              className="absolute h-96 aspect-[9/16] bg-center bg-cover rounded-2xl bg-no-repeat"
+            >
+              <div className="flex justify-between z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[140%]">
+                <motion.button
+                  initial={{ opacity: 0, scale: 0 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{
+                    type: "spring",
+                    duration: 0.5,
+                  }}
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.8 }}
+                  className="bwd-btn w-7 h-7 rounded-full bg-slate-700 text-white font-bold"
+                  onClick={prevBtn}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M15 19l-7-7 7-7"
+                    />
+                  </svg>
+                </motion.button>
+                <motion.button
+                  initial={{ opacity: 0, scale: 0 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{
+                    type: "spring",
+                    duration: 0.5,
+                  }}
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.8 }}
+                  className="fwd-btn w-7 h-7 rounded-full bg-slate-700 text-white font-bold"
+                  onClick={nextBtn}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9 5l7 7-7 7"
+                    />
+                  </svg>
+                </motion.button>
+              </div>
+            </motion.div>
+            <motion.div
+              key={rightId}
+              variants={variants}
+              initial={flowDirection ? "rightHidden" : "center"}
+              animate="right"
+              exit={"rightHidden"}
+              className="absolute h-96 aspect-[9/16] bg-center bg-cover rounded-2xl bg-no-repeat"
+            ></motion.div>
+          </AnimatePresence>
+        </motion.div>
+      </motion.div>
+    </div>
   )
 }
 
