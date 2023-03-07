e73781b85222829b96086f73ab9a16fb6f290a10
Devanshu Chatterjee <devanshu4190@gmail.com>
2023-03-08 02:36:56 +0530

    added compression to story page images to prevent memory overflow

--- src/components/slideshow/Carousel.jsx
+++ src/components/slideshow/Carousel.jsx
@@ -10,44 +10,50 @@ const Carousel = ({ data, view, title, links }) => {
   const [centerId, setCenterId] = useState(0)
   const [leftId, setLeftId] = useState(data.length - 1)
   const [rightId, setRightId] = useState(1)
+  const n = data.length
 
   const nextBtn = () => {
-    if (leftId === data.length - 1) {
-      setLeftId(0)
-    } else {
-      setLeftId(leftId + 1)
-    }
-    if (centerId === data.length - 1) {
-      setCenterId(0)
-    } else {
-      setCenterId(centerId + 1)
-    }
-    if (rightId === data.length - 1) {
-      setRightId(0)
-    } else {
-      setRightId(rightId + 1)
-    }
-
+    // if (leftId === data.length - 1) {
+    //   setLeftId(0)
+    // } else {
+    //   setLeftId(leftId + 1)
+    // }
+    // if (centerId === data.length - 1) {
+    //   setCenterId(0)
+    // } else {
+    //   setCenterId(centerId + 1)
+    // }
+    // if (rightId === data.length - 1) {
+    //   setRightId(0)
+    // } else {
+    //   setRightId(rightId + 1)
+    // }
+    setLeftId((prev) => (prev + 1) % n)
+    setCenterId((prev) => (prev + 1) % n)
+    setRightId((prev) => (prev + 1) % n)
     setFlowDirection(true)
   }
 
   const prevBtn = () => {
     setFlowDirection(false)
-    if (leftId === 0) {
-      setLeftId(data.length - 1)
-    } else {
-      setLeftId(leftId - 1)
-    }
-    if (centerId === 0) {
-      setCenterId(data.length - 1)
-    } else {
-      setCenterId(centerId - 1)
-    }
-    if (rightId === 0) {
-      setRightId(data.length - 1)
-    } else {
-      setRightId(rightId - 1)
-    }
+    // if (leftId === 0) {
+    //   setLeftId(data.length - 1)
+    // } else {
+    //   setLeftId(leftId - 1)
+    // }
+    // if (centerId === 0) {
+    //   setCenterId(data.length - 1)
+    // } else {
+    //   setCenterId(centerId - 1)
+    // }
+    // if (rightId === 0) {
+    //   setRightId(data.length - 1)
+    // } else {
+    //   setRightId(rightId - 1)
+    // }
+    setLeftId((prev) => (prev - 1 + n) % n)
+    setCenterId((prev) => (prev - 1 + n) % n)
+    setRightId((prev) => (prev - 1 + n) % n)
   }
 
   // useEffect(() => {
@@ -73,36 +79,36 @@ const Carousel = ({ data, view, title, links }) => {
     left: {
       x: "-20vw",
       opacity: 1,
-      filter: "brightness(40%)",
       scale: 1,
-      backgroundImage: "url(" + data[leftId] + ")",
       zIndex: "4",
-      boxShadow: "0px 0px 30px 0px rgba(0,0,0,0.0)",
+      filter: "brightness(40%)",
+      backgroundImage: "url(" + data[leftId] + ")",
+      boxShadow: "0px 0px 0px 0px rgba(0,0,0,0.0)",
       transition: {
         type: "spring",
         duration: 1,
       },
     },
     right: {
-      backgroundImage: "url(" + data[rightId] + ")",
       x: "20vw",
       opacity: 1,
-      filter: "brightness(40%)",
       scale: 1,
-      boxShadow: "0px 0px 30px 0px rgba(0,0,0,0.0)",
       zIndex: "3",
+      filter: "brightness(40%)",
+      backgroundImage: "url(" + data[rightId] + ")",
+      boxShadow: "0px 0px 0px 0px rgba(0,0,0,0.0)",
       transition: {
         type: "spring",
         duration: 1,
       },
     },
     rightHidden: {
-      x: "40vw",
+      x: "30vw",
       scale: 0,
       opacity: 0,
     },
     leftHidden: {
-      x: "-40vw",
+      x: "-30vw",
       scale: 0,
       opacity: 0,
     },
@@ -111,8 +117,9 @@ const Carousel = ({ data, view, title, links }) => {
   return (
     <div className="h-screen overflow-hidden">
       <div className="flex items-center justify-center pt-8 px-8 gap-4">
-        {data.map((d, i) => (
+        {Array.from(Array(n)).map((d, i) => (
           <div
+            key={i}
             className={`h-1 w-full rounded-md ${
               i <= centerId ? "bg-white" : "bg-gray-800"
             }`}
@@ -154,15 +161,7 @@ const Carousel = ({ data, view, title, links }) => {
                 </div>
               )}
               <div className="flex justify-between z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[140%]">
-                <motion.button
-                  initial={{ opacity: 0, scale: 0 }}
-                  animate={{ opacity: 1, scale: 1 }}
-                  transition={{
-                    type: "spring",
-                    duration: 0.5,
-                  }}
-                  whileHover={{ scale: 1.1 }}
-                  whileTap={{ scale: 0.8 }}
+                <button
                   className="bwd-btn w-7 h-7 rounded-full bg-slate-700 text-white font-bold"
                   onClick={prevBtn}
                 >
@@ -180,16 +179,8 @@ const Carousel = ({ data, view, title, links }) => {
                       d="M15 19l-7-7 7-7"
                     />
                   </svg>
-                </motion.button>
-                <motion.button
-                  initial={{ opacity: 0, scale: 0 }}
-                  animate={{ opacity: 1, scale: 1 }}
-                  transition={{
-                    type: "spring",
-                    duration: 0.5,
-                  }}
-                  whileHover={{ scale: 1.1 }}
-                  whileTap={{ scale: 0.8 }}
+                </button>
+                <button
                   className="fwd-btn w-7 h-7 rounded-full bg-slate-700 text-white font-bold"
                   onClick={nextBtn}
                 >
@@ -207,7 +198,7 @@ const Carousel = ({ data, view, title, links }) => {
                       d="M9 5l7 7-7 7"
                     />
                   </svg>
-                </motion.button>
+                </button>
               </div>
             </motion.div>
             <motion.div
--- src/pages/SlideShow.jsx
+++ src/pages/SlideShow.jsx
@@ -3,7 +3,6 @@ import { useParams } from "react-router-dom"
 import Carousel from "../components/slideshow/Carousel"
 import { createStore } from "polotno/model/store"
 import { Workspace } from "polotno/canvas/workspace"
-import axios from "axios"
 import { getStorySlugData } from "../components/adapters/story"
 
 const store = createStore({
@@ -20,12 +19,14 @@ const getImage = async (Json) => {
       if (x.hasOwnProperty("custom")) links.push(x.custom)
       else links.push({ link: "", text: "" })
 
-      let url = await store.toDataURL({ pageId: x.id })
+      let url = await store.toDataURL({
+        pageId: x.id,
+        quality: 0.5,
+        pixelRatio: 0.5,
+        mimeType: "image/jpeg",
+      })
       carouselData.push(url)
-      // console.log(url)
     }
-    // let url = await store.toDataURL({ pageId: store.pages[0].id });
-    // carouselData.push(url)
   } catch (e) {
     console.log(e)
   }
@@ -45,24 +46,12 @@ const SlideShow = () => {
     setLoading(false)
     console.log("Loading State False")
     console.log(data)
+    // console.log(carouselData)
     console.log(links)
   }
 
   useEffect(() => {
     storySlugData()
-    // axios({
-    //   method: "get",
-    //   url: `/story/slug/${slug}/get/`,
-    // })
-    //   .then((res) => {
-    //     setView(res.data.view_count)
-    //     setTitle(res.data.title)
-    //     getImage(res.data.json_data)
-    //   })
-    //   .then(() => {
-    //     setLoading(false)
-    //     console.log("Loading State False")
-    //   })
   }, [])
 
   return (
