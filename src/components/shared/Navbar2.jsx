53cf228de7be970247d39ba7fceb5d23de2f7970
Devanshu Chatterjee <devanshu4190@gmail.com>
2023-03-15 09:31:54 +0530

    added navbar to editor screen with story rename field

--- src/components/shared/Navbar2.jsx
+++ src/components/shared/Navbar2.jsx
@@ -1,16 +1,17 @@
 import React from "react"
 import logo from "../../assets/Logo.svg"
-import upgrade from "../../assets/Groupupgrade.svg"
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import Title from "../navbar/title"
 
 const Navbar2 = ({ id, setShowModal }) => {
   const navigate = useNavigate()
   return (
-    <div className="sticky top-0 border-b-2 flex justify-between items-center px-4 py-2 bg-white">
+    <div className="border-b-2 flex justify-between items-center px-4 py-2 bg-white">
       <div className="p-2 text-black text-lg font-bold flex items-center gap-2">
-        <img src={logo} alt="" />
-        Storyslide
+        <Link to="/" className="flex items-center gap-2">
+          <img src={logo} alt="" />
+          Storyslide
+        </Link>
         <p
           className="pl-5"
           style={{ fontWeight: "normal", cursor: "pointer" }}
@@ -27,11 +28,6 @@ const Navbar2 = ({ id, setShowModal }) => {
         >
           Generate Link
         </button>
-        <button className="bg-yellow-100 border border-yellow-800 text-yellow-800 p-2 rounded-lg flex gap-1 items-center">
-          <img src={upgrade} alt="" />
-          Upgrade
-        </button>
-        <div className="rounded-full bg-gray-200 w-8 h-8">P</div>
       </div>
     </div>
   )
